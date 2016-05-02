b3a24f5393942fa12239b2e3ee12527b62932f92
Stu Kabakoff <stuart@compstak.com>
2016-05-02 15:22:08 -0400

    use glob to check directories too

--- index.js
+++ index.js
@@ -5,9 +5,7 @@
  * @see https://gist.github.com/Morhaus/333579c2a5b4db644bd5
  */
 
-var path = require('path');
-var fs = require('fs');
-var _ = require('lodash');
+var glob = require('glob');
 
 function ForceCaseSensitivityPlugin() {
   //no-op
@@ -16,31 +14,14 @@ function ForceCaseSensitivityPlugin() {
 ForceCaseSensitivityPlugin.prototype.apply = function(compiler) {
   compiler.plugin('normal-module-factory', function(nmf) {
     nmf.plugin('after-resolve', function(data, done) {
-      var parentDir = path.dirname(data.resource);
-      var resourceName = path.basename(data.resource);
-      
-      //Ensure the file exists, it's possible we have a webpack-hot-loader file with get params that other webpack plugins understand
-      //e.g. .../node_modules/webpack-dev-server/client/index.js?http://localhost:3000
-      //We'll let webpack figure out if a file doesn't exist
-      fs.exists(parentDir + "/" + resourceName, function (exists) {
-        if (exists) {
-          fs.readdir(parentDir, function(err, files) {
-            if (err) {
-              done(err);
-            }
-            if (files.indexOf(resourceName) === -1) {
-              var realName = _.find(files, function(filename) {
-                return filename.toLowerCase() === resourceName.toLowerCase()
-              });
-              done(new Error('ForceCaseSensitivityPlugin: `' + resourceName + '` does not match the corresponding file on disk `' + realName + '`'));
-              return;
-            }
-            done(null, data);
-          });
-        } else {
-          done(null, data);
-        }
-      });
+
+      var realpath = glob.sync(data.resource, {nocase: true})[0];
+
+      if (realpath !== data.resource) {
+        done(new Error('ForceCaseSensitivityPlugin: `' + data.resource + '` does not match the corresponding file on disk `' + realpath + '`'));
+      } else {
+        done(null, data);
+      }
 
     });
   });
