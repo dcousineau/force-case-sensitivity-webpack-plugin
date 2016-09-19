66e8447f6719bdc39bce7f5231251975e7ad5d85
Daniel Cousineau <dcousineau@gmail.com>
2016-09-19 15:17:58 -0400

    Updates npm version, fixes issue with hotloader style includes.
    Also improves error output to show the raw include string the user typed.

--- index.js
+++ index.js
@@ -8,23 +8,28 @@
 var glob = require('glob');
 
 function ForceCaseSensitivityPlugin() {
-  //no-op
+    //no-op
 }
 
 ForceCaseSensitivityPlugin.prototype.apply = function(compiler) {
-  compiler.plugin('normal-module-factory', function(nmf) {
-    nmf.plugin('after-resolve', function(data, done) {
+    compiler.plugin('normal-module-factory', function(nmf) {
+        nmf.plugin('after-resolve', function(data, done) {
 
-      var realpath = glob.sync(data.resource, {nocase: true})[0];
+            var realpath = glob.sync(data.resource, {nocase: true})[0];
 
-      if (realpath !== data.resource) {
-        done(new Error('ForceCaseSensitivityPlugin: `' + data.resource + '` does not match the corresponding file on disk `' + realpath + '`'));
-      } else {
-        done(null, data);
-      }
+            //If realpath is undefined that means glob could not find the file. This
+            //was most likely an include likely due to either (A) the file doesn't
+            //exist or (B) webpack-dev-server/client?http://localhost:1111/ or
+            //similar. Other systems will throw a fatal error if it's case A, so we
+            //ignore files that "don't exist"
+            if (realpath && realpath !== data.resource) {
+                done(new Error('ForceCaseSensitivityPlugin: `' + data.rawRequest + '` does not match the corresponding file on disk `' + realpath + '`'));
+            } else {
+                done(null, data);
+            }
 
+        });
     });
-  });
 };
 
 module.exports = ForceCaseSensitivityPlugin;
